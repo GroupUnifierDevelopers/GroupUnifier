ebf78bf738b6e8dcdc66a8aba66f64be1f7e0fdf
Rubicman <kubantsev@yandex.ru>
2021-10-13 20:44:04 +0300

    Correction of pull request comments

--- src/test/kotlin/ru/itmo/sofwaredesign/groupunifier/common/ForwarderTest.kt
+++ src/test/kotlin/ru/itmo/sofwaredesign/groupunifier/common/ForwarderTest.kt
@@ -8,110 +8,69 @@ import ru.itmo.sofwaredesign.groupunifier.common.domain.Messenger
 import ru.itmo.sofwaredesign.groupunifier.common.domain.User
 import ru.itmo.sofwaredesign.groupunifier.common.repository.ChatRepository
 
+private const val TEST_MESSAGE = "test massage"
+private const val TEST_USER = "testUser"
+private const val FROM_TEST_CHAT_ID = "from test chat id"
+private const val TO_TEST_CHAT_ID = "to test chat id"
+private const val TEST_BOT_LOGIN = "testBotLogin"
+
 class ForwarderTest {
 
     @Test
-    fun forwardTest() {
-        val fromMockMessenger: Messenger = mock()
-        val message = Message(
-            "test message",
-            User("testUser", fromMockMessenger),
-            "from chat id",
-            fromMockMessenger
-        )
-        val toMockMessenger: Messenger = mock()
-        val chat = Chat()
-        chat.implementations[toMockMessenger] = "to chat id"
-        val mockChatRepository: ChatRepository = mock()
+    fun forwardTest() = getTestData().run {
         whenever(
             mockChatRepository
-                .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+                .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         )
             .thenReturn(chat)
 
-        val forwarder = Forwarder(mockChatRepository)
         forwarder.forwardMessage(message)
 
         verify(mockChatRepository)
-            .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+            .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         verify(toMockMessenger)
-            .sendMessage("to chat id", message)
+            .sendMessage(TO_TEST_CHAT_ID, message)
     }
 
     @Test
-    fun noChatTest() {
-        val fromMockMessenger: Messenger = mock()
-        val message = Message(
-            "test message",
-            User("testUser", fromMockMessenger),
-            "from chat id",
-            fromMockMessenger
-        )
-        val toMockMessenger: Messenger = mock()
-        val chat = Chat()
-        chat.implementations[toMockMessenger] = "to chat id"
-        val mockChatRepository: ChatRepository = mock()
+    fun noChatTest() = getTestData().run {
         whenever(
             mockChatRepository
-                .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+                .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         )
             .thenReturn(null)
 
-        val forwarder = Forwarder(mockChatRepository)
         forwarder.forwardMessage(message)
 
         verify(mockChatRepository)
-            .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+            .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         verify(toMockMessenger, times(0))
-            .sendMessage("to chat id", message)
+            .sendMessage(TO_TEST_CHAT_ID, message)
     }
 
     @Test
-    fun fromBotMessageTest() {
-        val fromMockMessenger: Messenger = mock()
-        whenever(fromMockMessenger.botLogin)
-            .thenReturn("botLogin")
-        val message = Message(
-            "test message",
-            User("botLogin", fromMockMessenger),
-            "from chat id",
-            fromMockMessenger
-        )
-        val toMockMessenger: Messenger = mock()
-        val chat = Chat()
-        chat.implementations[toMockMessenger] = "to chat id"
-        val mockChatRepository: ChatRepository = mock()
+    fun fromBotMessageTest() = getTestData(
+        userLogin = TEST_BOT_LOGIN
+    ).run {
         whenever(
             mockChatRepository
-                .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+                .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         )
             .thenReturn(null)
 
-        val forwarder = Forwarder(mockChatRepository)
         forwarder.forwardMessage(message)
 
         verify(mockChatRepository, times(0))
-            .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+            .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         verify(toMockMessenger, times(0))
-            .sendMessage("to chat id", message)
+            .sendMessage(TO_TEST_CHAT_ID, message)
     }
 
     @Test
-    fun retrySuccessTest() {
-        val fromMockMessenger: Messenger = mock()
-        val message = Message(
-            "test message",
-            User("testUser", fromMockMessenger),
-            "from chat id",
-            fromMockMessenger
-        )
-        val toMockMessenger: Messenger = mock()
-        val chat = Chat()
-        chat.implementations[toMockMessenger] = "to chat id"
-        val mockChatRepository: ChatRepository = mock()
+    fun retrySuccessTest() = getTestData().run {
         whenever(
             mockChatRepository
-                .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+                .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         )
             .thenReturn(chat)
         whenever(toMockMessenger.sendMessage(any(), any()))
@@ -120,42 +79,65 @@ class ForwarderTest {
             .thenThrow(RuntimeException::class.java)
             .then {}
 
-        val forwarder = Forwarder(mockChatRepository)
         forwarder.forwardMessage(message)
 
         verify(mockChatRepository)
-            .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+            .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         verify(toMockMessenger, times(4))
-            .sendMessage("to chat id", message)
+            .sendMessage(TO_TEST_CHAT_ID, message)
     }
 
     @Test
-    fun retryFailTest() {
-        val fromMockMessenger: Messenger = mock()
-        val message = Message(
-            "test message",
-            User("testUser", fromMockMessenger),
-            "from chat id",
-            fromMockMessenger
-        )
-        val toMockMessenger: Messenger = mock()
-        val chat = Chat()
-        chat.implementations[toMockMessenger] = "to chat id"
-        val mockChatRepository: ChatRepository = mock()
+    fun retryFailTest() = getTestData().run {
         whenever(
             mockChatRepository
-                .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+                .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         )
             .thenReturn(chat)
         whenever(toMockMessenger.sendMessage(any(), any()))
             .thenThrow(RuntimeException::class.java)
 
-        val forwarder = Forwarder(mockChatRepository)
         forwarder.forwardMessage(message)
 
         verify(mockChatRepository)
-            .getChatByMessengerAndId(fromMockMessenger, "from chat id")
+            .getChatByMessengerAndId(fromMockMessenger, FROM_TEST_CHAT_ID)
         verify(toMockMessenger, times(5))
-            .sendMessage("to chat id", message)
+            .sendMessage(TO_TEST_CHAT_ID, message)
     }
+
+    private fun getTestData(
+        userLogin: String = TEST_USER
+    ): TestData {
+        val fromMockMessenger: Messenger = mock()
+        whenever(fromMockMessenger.botLogin)
+            .thenReturn(TEST_BOT_LOGIN)
+        val message = Message(
+            TEST_MESSAGE,
+            User(userLogin, fromMockMessenger),
+            FROM_TEST_CHAT_ID,
+            fromMockMessenger
+        )
+        val toMockMessenger: Messenger = mock()
+        val chat = Chat()
+        chat.implementations[toMockMessenger] = TO_TEST_CHAT_ID
+        val mockChatRepository: ChatRepository = mock()
+        val forwarder = Forwarder(mockChatRepository)
+        return TestData(
+            fromMockMessenger,
+            message,
+            toMockMessenger,
+            chat,
+            mockChatRepository,
+            forwarder
+        )
+    }
+
+    private data class TestData constructor(
+        val fromMockMessenger: Messenger,
+        val message: Message,
+        val toMockMessenger: Messenger,
+        val chat: Chat,
+        val mockChatRepository: ChatRepository,
+        val forwarder: Forwarder
+    )
 }
